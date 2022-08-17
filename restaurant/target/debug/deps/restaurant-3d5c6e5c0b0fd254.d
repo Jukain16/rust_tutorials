1638e8257e88668133774b3d82e3cfd9ca6f5316
Sarah Nurse <sarah@localhost.localdomain>
2022-08-17 19:22:35 -0400

    Added chapter 7 project

---
+++ restaurant/target/debug/deps/restaurant-3d5c6e5c0b0fd254.d
@@ -0,0 +1,5 @@
+/home/sarah/Code/Rust/restaurant/target/debug/deps/restaurant-3d5c6e5c0b0fd254.rmeta: src/main.rs
+
+/home/sarah/Code/Rust/restaurant/target/debug/deps/restaurant-3d5c6e5c0b0fd254.d: src/main.rs
+
+src/main.rs:
