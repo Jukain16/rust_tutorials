c6d497eb8bddb6aef6cb92bb602a0d4be9fb87ee
Sarah Nurse <sarah@localhost.localdomain>
2022-08-18 20:25:31 -0400

    Started chapter 8 project

---
+++ collections/target/debug/deps/mistakes-8f97d7fa64941e13.d
@@ -0,0 +1,5 @@
+/home/sarah/Code/Rust/collections/target/debug/deps/mistakes-8f97d7fa64941e13.rmeta: src/main.rs
+
+/home/sarah/Code/Rust/collections/target/debug/deps/mistakes-8f97d7fa64941e13.d: src/main.rs
+
+src/main.rs:
