fca8a21919acffae40f7e5ccb675feeb1f2cb5a6
Sarah <sarah@localhost.localdomain>
2022-09-10 15:03:23 -0400

    Added averages

---
+++ averages/target/debug/deps/averages-9221b66d268b26a1.d
@@ -0,0 +1,5 @@
+/home/sarah/Code/Rust/averages/target/debug/deps/averages-9221b66d268b26a1.rmeta: src/main.rs
+
+/home/sarah/Code/Rust/averages/target/debug/deps/averages-9221b66d268b26a1.d: src/main.rs
+
+src/main.rs:
