99065b61faa6dad2e9d50d6bd0c2d18e54b95f6a
Sarah <sarah@localhost.localdomain>
2022-09-10 13:05:04 -0400

    Added gitignore

---
+++ points/target/debug/deps/points-ce6a6982ee3f5faf.d
@@ -0,0 +1,5 @@
+/home/sarah/Code/Rust/points/target/debug/deps/points-ce6a6982ee3f5faf.rmeta: src/main.rs
+
+/home/sarah/Code/Rust/points/target/debug/deps/points-ce6a6982ee3f5faf.d: src/main.rs
+
+src/main.rs:
