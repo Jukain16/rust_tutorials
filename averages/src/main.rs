d48870817f9f656191fbd245f6e422101e1b4bd6
Sarah <sarah@localhost.localdomain>
2022-09-17 23:03:19 -0400

    I hate vectors my god

--- averages/src/main.rs
+++ averages/src/main.rs
@@ -1,4 +1,9 @@
 fn main() {
    let v: Vec<i32> = Vec::new(); 
-   let mut v = vec![14, 20, 304, 45, 3, 3, 2, 14];
+   let v = vec![14, 20, 304, 45, 3, 3, 2, 14];
+   for mut i in v {
+    i += i;
+   }
+   let mean = i / 8;
+   println!("mean is {}", mean);
 }
