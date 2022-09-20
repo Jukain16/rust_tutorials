986d7353b15878660f81cda7a3040e057f9872f5
Sarah <sarah@localhost.localdomain>
2022-09-19 20:45:53 -0400

    idk what I'm doing anymore

--- averages/src/main.rs
+++ averages/src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
-   let v: Vec<i32> = Vec::new(); 
    let v = vec![14, 20, 304, 45, 3, 3, 2, 14];
-   for mut i in &mut v {
-       *i += i;
+   for mut i in v {
+       i += i;
+       let mean = i / 8;
+      println!("The mean is {}", mean);
    }
 }
