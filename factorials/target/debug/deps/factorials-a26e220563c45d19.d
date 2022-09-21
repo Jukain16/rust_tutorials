cd71275584a92d251805fb6df30ece0482099757
Sarah <sarah@localhost.localdomain>
2022-09-20 21:08:11 -0400

    Added factorials

---
+++ factorials/target/debug/deps/factorials-a26e220563c45d19.d
@@ -0,0 +1,5 @@
+/home/sarah/Code/Rust/factorials/target/debug/deps/factorials-a26e220563c45d19.rmeta: src/main.rs
+
+/home/sarah/Code/Rust/factorials/target/debug/deps/factorials-a26e220563c45d19.d: src/main.rs
+
+src/main.rs:
