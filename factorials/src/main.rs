79aefc60cbe2a4a72f799e6d654b6bdc18380a68
Sarah <sarah@localhost.localdomain>
2022-09-23 19:25:46 -0400

    Finally figured out factorials

--- factorials/src/main.rs
+++ factorials/src/main.rs
@@ -3,7 +3,7 @@ fn factorial(num: u32) -> u32 {
     if num <= 1 {
         return 1;
     }
-    num * factorial(num - 1);
+    num * factorial(num - 1)
 }
 fn main() {
     println!("Enter positive interger.");
