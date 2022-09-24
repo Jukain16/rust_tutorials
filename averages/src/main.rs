87e547a79cdbcc2e35e7e29c818277ef31b93683
Sarah <sarah@localhost.localdomain>
2022-09-24 09:39:16 -0400

    Added functions for mean, median and mode

--- averages/src/main.rs
+++ averages/src/main.rs
@@ -1,6 +1,18 @@
-fn main() {
+fn mean() {
    let v = vec![14, 20, 304, 45, 3, 3, 2, 14];
    let n = v[0] + v[1] + v[2] + v[3] + v[4] + v[5] + v[6] + v[7];
    let mean = n / 8;
    println!("{}", mean);
+}
+
+fn median() {
+
+}
+
+fn mode() {
+
+}
+
+fn main() {
+
 }
