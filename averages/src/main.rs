30add2fba4aa84f88df08b6d8b4793ad8550ba69
Sarah <sarah@localhost.localdomain>
2022-09-24 09:37:53 -0400

    Solved mean problem

--- averages/src/main.rs
+++ averages/src/main.rs
@@ -1,8 +1,6 @@
 fn main() {
    let v = vec![14, 20, 304, 45, 3, 3, 2, 14];
-   for mut i in v {
-       i += i;
-       let mean = i / 8;
-      println!("The mean is {}", mean);
-   }
+   let n = v[0] + v[1] + v[2] + v[3] + v[4] + v[5] + v[6] + v[7];
+   let mean = n / 8;
+   println!("{}", mean);
 }
