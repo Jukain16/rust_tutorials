ddc48148494688b6ee5ec417b847c6db4a863514
Sarah <sarah@localhost.localdomain>
2022-09-24 13:40:51 -0400

    I hate vectors so much

--- averages/src/main.rs
+++ averages/src/main.rs
@@ -1,20 +1,23 @@
-fn mean() {
+use std::collections::HashMap;
+fn mean(&Vec<i32>) {
    let n = v[0] + v[1] + v[2] + v[3] + v[4] + v[5] + v[6] + v[7];
    let mean = n / 8;
    println!("{}", mean);
 }
 
-fn median() {
+fn median(&Vec<i32>) {
     let o = v[4] + v[5];
     let median = o / 2;
     println!("{}", median);
 }
 
-fn mode() {
+fn mode(&Vec<i32>) {
+  let mut mode = HashMap::new();
 }
 
 fn main() {
     let v = vec![14, 20, 304, 45, 3, 3, 2, 14];
-    mean();
-    median();
+    mean(&v);
+    median(&v);
+    mode(&v);
 }
