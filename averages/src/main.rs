b268a0e211c9d2601b2126e628021e2ca0fd05bf
Sarah <sarah@localhost.localdomain>
2022-09-25 11:00:18 -0400

    Finally solved this thank stack overflow

--- averages/src/main.rs
+++ averages/src/main.rs
@@ -1,25 +1,36 @@
 use std::collections::HashMap;
-fn mean(v: Vec<u32>) {
-   let n = v[0] + v[1] + v[2] + v[3] + v[4] + v[5] + v[6] + v[7];
-   let mean = n / 8;
-   println!("{}", mean);
-}
-
-fn median(v: Vec<u32>) {
-    let o = v[7] + v[0];
-    let median = o / 2;
-    println!("{}", median);
-}
-
-fn mode(v: Vec<u32>) {
-  let mut mode = HashMap::new();
-  mode.insert(3, 14);
-  println!("{:?}", mode);
-}
-
 fn main() {
-    let v = vec![14, 20, 304, 45, 3, 3, 2, 14];
-    mean(v);
-    median(v);
-    mode(v);
+    let mut v = vec![14, 20, 304, 45, 3, 3, 2, 14];
+    let mean: f32;
+    let median: i32;
+    let mode: i32;
+    {
+        let mut sum = 0;
+        for i in &v {
+            sum += i;
+        }
+        mean = sum as f32 / v.len() as f32;
+    }
+    {
+        v.sort();
+        let mid = v.len() / 2;
+        median = v[mid];
+    }
+    {
+        let mut times = HashMap::new();
+        for i in &v {
+            let cnt = times.entry(*i as usize).or_insert(0);
+            *cnt += 1;
+        }
+        let mut frequent: (i32, i32) = (*times.iter().nth(0)
+            .expect("Fatal.").0 as i32, *times.iter().nth(0)
+            .expect("Fatal.").1 as i32);
+            for i in times.iter() {
+                if *i.1 > frequent.1 {
+                    frequent = (*i.0 as i32, *i.1);
+                }
+            }
+        mode = frequent.0;
+    }
+    println!("The mean is {}, The median is {}, and the mode is {}", mean, median, mode);
 }
